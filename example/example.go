56271714a7ed1d14a929b98decf508e566ff877c
Elliot Wright <wright.elliot@gmail.com>
2019-12-15 02:02:55 +0000

    Tidied up example code. Validation rules still are't very sensible. It's not a great example, will update soon.

--- example/example.go
+++ example/example.go
@@ -45,37 +45,59 @@ func (e Example) Constraints(t valley.Type) {
 	t.Constraints(constraints.MutuallyInclusive(e.Int, e.Int2, e.Ints))
 
 	// Field constraints.
-	t.Field(e.Bool).Constraints(constraints.NotEquals(false), constraints.DeepEquals(true))
-	t.Field(e.Chan).Constraints(constraints.MaxLength(12))
-	t.Field(e.Text).Constraints(constraints.Required(), constraints.RegexpString("^Hello"))
-	t.Field(e.Text).Constraints(constraints.Predicate(
-		strings.HasPrefix(e.Text, "custom") && len(e.Text) == 32,
-		"value must be a valid custom ID",
-	))
-	t.Field(e.Text).Constraints(constraints.MaxLength(12), constraints.Length(5))
-	t.Field(e.TextMap).Constraints(constraints.Required()).
+	t.Field(e.Bool).
+		Constraints(constraints.NotEquals(false), constraints.DeepEquals(true))
+	t.Field(e.Chan).
+		Constraints(constraints.MaxLength(12))
+	t.Field(e.Text).
+		Constraints(
+			constraints.Required(),
+			constraints.RegexpString("^Hello"),
+			constraints.MaxLength(12),
+			constraints.Length(5),
+			constraints.Predicate(
+				strings.HasPrefix(e.Text, "custom") && len(e.Text) == 32,
+				"value must be a valid custom ID",
+			),
+		)
+	t.Field(e.TextMap).
+		Constraints(constraints.Required()).
 		Elements(constraints.Required()).
 		Keys(constraints.MinLength(10))
-	t.Field(e.Int).Constraints(constraints.Required())
-	t.Field(e.Int2).Constraints(constraints.Required(), constraints.NotNil(), constraints.Min(0))
-	t.Field(e.Ints).Constraints(constraints.Required(), constraints.MaxLength(3)).
+	t.Field(e.Int).
+		Constraints(constraints.Required())
+	t.Field(e.Int2).
+		Constraints(constraints.Required(), constraints.NotNil(), constraints.Min(0))
+	t.Field(e.Ints).
+		Constraints(constraints.Required(), constraints.MaxLength(3)).
 		Elements(constraints.Required(), constraints.Min(0))
-	t.Field(e.Float).Constraints(constraints.Equals(math.Pi))
-	t.Field(e.Time).Constraints(constraints.TimeBefore(timeYosemite))
-	t.Field(e.Times).Constraints(constraints.MinLength(1)).
+	t.Field(e.Float).
+		Constraints(constraints.Equals(math.Pi))
+	t.Field(e.Time).
+		Constraints(constraints.TimeBefore(timeYosemite))
+	t.Field(e.Times).
+		Constraints(constraints.MinLength(1)).
 		Elements(constraints.TimeBefore(timeYosemite))
-	t.Field(e.Adults).Constraints(constraints.Min(1), constraints.Max(9))
-	t.Field(e.Children).Constraints(constraints.Min(0), constraints.Equals(e.Adults+2)).
-		Constraints(constraints.Max(int(math.Max(float64(8-(e.Adults-1)), 0))))
+	t.Field(e.Adults).
+		Constraints(constraints.Min(1), constraints.Max(9))
+	t.Field(e.Children).
+		Constraints(
+			constraints.Min(0),
+			constraints.Equals(e.Adults+2),
+			constraints.Max(int(math.Max(float64(8-(e.Adults-1)), 0))),
+		)
 
 	// Conditional constraints.
 	t.When(len(e.Text) > 32).Field(e.Text).
 		Constraints(constraints.Required(), constraints.MinLength(64))
 
 	// Nested constraints to be called.
-	t.Field(e.Nested).Constraints(constraints.Required(), constraints.Valid())
-	t.Field(e.Nesteds).Elements(constraints.Valid())
-	t.Field(e.NestedMap).Keys(constraints.Valid())
+	t.Field(e.Nested).
+		Constraints(constraints.Required(), constraints.Valid())
+	t.Field(e.Nesteds).
+		Elements(constraints.Valid())
+	t.Field(e.NestedMap).
+		Keys(constraints.Valid())
 }
 
 // NestedExample ...
