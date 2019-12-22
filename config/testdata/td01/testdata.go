d4349c73b92461903a27fcc43c35ddd8ceb5c10b
Elliot Wright <wright.elliot@gmail.com>
2019-12-22 00:22:44 +0000

    Updated config test to touch more of the config building code.

--- config/testdata/td01/testdata.go
+++ config/testdata/td01/testdata.go
@@ -9,16 +9,33 @@ import (
 
 // Subject is a type used for testing source reading functionality.
 type Subject struct {
-	SomeText string   `json:"some_text"`
-	SomeBool bool     `json:"some_bool"`
-	SomePtr  *Subject `json:"some_ptr"`
+	SomeText  string         `json:"some_text"`
+	SomeBool  bool           `json:"some_bool"`
+	SomePtr   *Subject       `json:"some_ptr"`
+	SomeSlice []string       `json:"some_slice"`
+	SomeMap   map[string]int `json:"some_map"`
 }
 
 // Constraints is a valley constraints method used for testing source reading functionality.
 func (s Subject) Constraints(t valley.Type) {
-	t.Field(s.SomeText).Constraints(c.Required())
-	t.Field(s.SomeBool).Constraints(c.Equals(true))
-	t.Field(s.SomePtr).Constraints(c.NotNil())
+	t.Constraints(c.MutuallyExclusive(s.SomeSlice, s.SomeMap))
+
+	t.Field(s.SomeText).
+		Constraints(c.Required())
+	t.Field(s.SomeBool).
+		Constraints(c.Equals(true))
+	t.Field(s.SomePtr).
+		Constraints(c.NotNil())
+	t.Field(s.SomeSlice).
+		Constraints(c.MinLength(1), c.MaxLength(128)).
+		Elements(c.MinLength(1), c.MaxLength(32))
+	t.Field(s.SomeMap).
+		Constraints(c.MinLength(1)).
+		Elements(c.Min(1)).
+		Keys(c.MinLength(3))
+
+	t.When(s.SomeBool).Field(s.SomePtr).
+		Constraints(c.NotNil())
 }
 
 // SecondarySubject is a type used for testing source reading functionality.
@@ -33,20 +50,4 @@ func (s *SecondarySubject) Constraints(t valley.Type) {
 	t.Field(s.SomeText).Constraints(c.Required())
 	t.Field(s.SomeBool).Constraints(c.Equals(true))
 	t.Field(s.SomePtr).Constraints(c.NotNil())
-}
-
-// SomeVar is used to ensure generic declarations that aren't types aren't included in the
-// information read from Go source files when testing source reading functionality.
-var SomeVar = 123
-
-// SomeInterface is used to ensure interfaces aren't included in the information read from Go source
-// files when testing source reading functionality.
-type SomeInterface interface {
-	ThatIsUnused()
-}
-
-// SomeFunction is used to ensure function declarations that aren't methods aren't included in the
-// information read from Go source files when testing source reading functionality.
-func SomeFunction() {
-	// No-op.
 }
